9acd82e617aded5f86e905852cdc74f230f74b16
Benito Gonzalez <ohnobenito@gmail.com>
2020-11-04 14:38:04 -0600

    Getting currentweather to display on page

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,20 +1,50 @@
+//WHEN SEARCH BUTTON IS CLICKED
 $("#search-btn").on("click", function(){
     displayWeather();
+    
 })
+
 function displayWeather() {
     let city = $("#search-term").val();
     let apiKey = "&appid=663280f624e2f9932dc29f35de7ca316";
-    let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+    let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial"+ apiKey;
 
     $.ajax({
         url: queryUrl,
         method: "GET"
     }).then(function (response) {
-        console.log(response)
-        $("#current-weather").text(JSON.stringify(response));
-    })
-}
+        //RESPONSE RESULTS
+            
+            let name = response.name;
+            let temp = (response.main.temp)
+            let humidity = response.main.humidity;
+            let wind = response.wind.speed;
+            let icon = response.weather[0].icon;
+            
 
+        // BUILDING THE DIV
+        let weatherDiv = $("<div class='weather'>")
+        let h1 = $("<h1>").text(name);  
+        weatherDiv.append(h1);
+
+        temp = Math.floor(temp);
+        let pOne = $("<p>").text("Temp: " + temp);
+        weatherDiv.append(pOne);
+
+        let pTwo = $("<p>").text("Humidity: " + humidity);
+        weatherDiv.append(pTwo);
+
+        wind = Math.floor(wind);
+        let pThree = $("<p>").text("Wind Speed: " + wind + " MPH");
+        weatherDiv.append(pThree);
+
+        let iconIm = $("<img>").attr("src", "https://openweathermap.org/img/w/" + icon + ".png");
+        weatherDiv.append(iconIm);
+
+
+$("#current-weather").prepend(weatherDiv);
+})
+}; 
 
 //ADD SEARCH RESULTS TO ONGOING "past-search" DIV
 
